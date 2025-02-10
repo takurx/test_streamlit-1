11e9f0c0b2ef35454dda73e8fb4541c2268416dd
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-10 16:16:10 +0900

    try hand over value decmal <--> hex
    try hand over value decmal <--> hex

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ import streamlit as st
 
 st.title('Hello World!')
 
-number = st.text_input('Please decimal number', '0')
+number = '0'
+number = st.text_input('Please decimal number', number)
+
+hex_number = '0x0'
+hex_number = st.text_input('Please hexadecimal number', hex_number)
 
 if number.isnumeric():
-    square_number = int(number) * int(number)
-    #st.write('Square of your input number is', str(square_number))
     st.write('Hexadecimal is your input number is', hex(int(number)))
-else:
-    st.write('Please input number')
-
-hex_number = st.text_input('Please hexadecimal number', '0x0')
-
-if hex_number.startswith('0x') and hex_number[2:].isalnum():
+    hex_number = hex(int(number))
+elif hex_number.startswith('0x') and hex_number[2:].isalnum():
     decimal_number = int(hex_number, 16)
     st.write('Decimal of your input number is', str(decimal_number))
+    number = str(decimal_number)
 else:
-    st.write('Please input hexadecimal number')
+    st.write('Please input number or Please input hexadecimal number')
 
