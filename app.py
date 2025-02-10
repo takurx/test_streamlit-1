acfc4b73e06a041f2805215b7fd1ea917c4d671d
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-10 16:06:38 +0900

    try convert decimal t o hexdecimal
    try convert decimal t o hexdecimal

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ number = st.text_input('Please input number', '0')
 
 if number.isnumeric():
     square_number = int(number) * int(number)
-    st.write('Square of your input number is', str(square_number))
+    #st.write('Square of your input number is', str(square_number))
+    st.write('Hexadecimal is your input number is', hex(int(number)))
 else:
     st.write('Please input number')
 
