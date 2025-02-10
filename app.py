e8fc04353227af79ef8be0bc387d59f50527f0a8
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-10 16:33:08 +0900

    return to V1
    return to V1

--- app.py
+++ app.py
@@ -5,24 +5,18 @@ import streamlit as st
 
 st.title('Hello World!')
 
-st.session_state.number = '0'
-number = st.text_input('Please decimal number', value=st.session_state.number)
+number = st.text_input('Please decimal number', value='0')
 
-st.session_state.hex_number = '0x0'
-hex_number = st.text_input('Please hexadecimal number', value=st.session_state.hex_number)
+if number.isnumeric():
+    st.write('Hexadecimal is your input number is', hex(int(number)))
+else:
+    st.write('Please input number')
 
-btn = st.button('Convert')
+hex_number = st.text_input('Please hexadecimal number', value='0x0')
 
-if btn:
-    if number.isnumeric():
-        st.write('Hexadecimal is your input number is', hex(int(number)))
-        st.session_state.hex_number = hex(int(number))
-    elif hex_number.startswith('0x') and hex_number[2:].isalnum():
-        decimal_number = int(hex_number, 16)
-        st.write('Decimal of your input number is', str(decimal_number))
-        st.session_state.number = str(decimal_number)
-    else:
-        st.write('Please input number or Please input hexadecimal number')
-    #None
-# None
+if hex_number.startswith('0x') and hex_number[2:].isalnum():
+    decimal_number = int(hex_number, 16)
+    st.write('Decimal of your input number is', str(decimal_number))
+else:
+    st.write('Please input hexadecimal number')
 
