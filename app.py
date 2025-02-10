eb568397a5afc7571a7245f13fe9c4750727e66b
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-10 16:09:46 +0900

    try convert hexadecimal to decimal
    try convert hexadecimal to decimal

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.title('Hello World!')
 
-number = st.text_input('Please input number', '0')
+number = st.text_input('Please decimal number', '0')
 
 if number.isnumeric():
     square_number = int(number) * int(number)
@@ -14,4 +14,11 @@ if number.isnumeric():
 else:
     st.write('Please input number')
 
+hex_number = st.text_input('Please hexadecimal number', '0x0')
+
+if hex_number.startswith('0x') and hex_number[2:].isalnum():
+    decimal_number = int(hex_number, 16)
+    st.write('Decimal of your input number is', str(decimal_number))
+else:
+    st.write('Please input hexadecimal number')
 
