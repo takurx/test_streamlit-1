74effc44eb50c1a636d04fb8375f61927ade2889
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-11 15:27:58 +0900

    add decription test box
    add decription test box

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import streamlit as st
 
 st.title('Covnert to 16->10 and 10->16, V1.0')
 
-number = st.text_input('Please decimal number', value='0')
+number = st.text_input('Please input decimal number (10 base number)', value='0')
 
 if number.isnumeric():
     st.write('Hexadecimal is your input number is', hex(int(number)))
 else:
     st.write('Please input number')
 
-hex_number = st.text_input('Please hexadecimal number', value='0x0')
+hex_number = st.text_input('Please input hexadecimal number (16 base number)', value='0x0')
 
 if hex_number.startswith('0x') and hex_number[2:].isalnum():
     decimal_number = int(hex_number, 16)
