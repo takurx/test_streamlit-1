c50e923b2a694c957fa0ca3fddeff5c78c4741bc
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-08-08 06:32:11 +0000

    add converted value, red color

--- app.py
+++ app.py
@@ -3,20 +3,20 @@
 
 import streamlit as st
 
-st.title('Covnert to 16->10 and 10->16, V1.0')
+st.title('Covnert to 16->10 and 10->16, V1.1')
 
 number = st.text_input('Please input decimal number (10 base number)', value='0')
 
 if number.isnumeric():
-    st.write('Hexadecimal is your input number is', hex(int(number)))
+    st.markdown("Hexadecimal is your input number is " + f":red[{hex(int(number))}]")
 else:
-    st.write('Please input number')
+    st.markdown("Please input number")
 
 hex_number = st.text_input('Please input hexadecimal number (16 base number)', value='0x0')
 
 if hex_number.startswith('0x') and hex_number[2:].isalnum():
     decimal_number = int(hex_number, 16)
-    st.write('Decimal of your input number is', str(decimal_number))
+    st.markdown("Decimal of your input number is " + f":red[{str(decimal_number)}]")
 else:
-    st.write('Please input hexadecimal number')
+    st.markdown("Please input hexadecimal number")
 
